0e03b34f3689b0865ec4f5293dad9dbf4133df76
Ian Alexander and Tom McElhinny <ian@>
2019-05-20 20:41:46 -0700

    it will add buttons and dark screen options

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,40 @@
 $(document).ready(function() {
   $(".clickable").click(function(){
-    $(".axolotl-showing").slideDown();
-    $(".axolotl-hidden").fadeToggle();
+    $(".axolotl-showing").toggle();
+    $(".axolotl-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
+$(document).ready(function() {
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+    $("button#light").toggle();
+    $("button#dark").toggle();
+  });
+
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+    $("body").addClass("darkfont");
+    $("button#dark").toggle();
+    $("button#light").toggle();
+    // $("button#light").btn color(white);
   });
 });
